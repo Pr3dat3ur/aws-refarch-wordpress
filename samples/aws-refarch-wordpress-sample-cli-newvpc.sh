9822401db65089d6ff3ca16e777617c042566f37
Pr3dat3ur <Pr3dat3ur@hotmail.com>
2021-11-28 21:29:41 +0100

    lol

--- samples/aws-refarch-wordpress-sample-cli-newvpc.sh
+++ samples/aws-refarch-wordpress-sample-cli-newvpc.sh
@@ -1,6 +1,6 @@
 aws cloudformation create-stack \
  --stack-name WordPress \
- --template-body file:///home/fab/Public/gitfolder/aws-refarch-wordpress/templates/aws-refarch-wordpress-master-newvpc.yaml \
+ --template-body file://https://cloyudformation.s3.eu-west-3.amazonaws.com/aws-refarch-wordpress/templates/aws-refarch-wordpress-master-newvpc.yaml \
  --parameters file://aws-refarch-wordpress-parameters-newvpc.json \
  --capabilities CAPABILITY_IAM \
  --disable-rollback \
